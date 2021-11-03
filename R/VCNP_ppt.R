cc1ce7b3b61ca931e2068e4fe61dae2eb5c8cb42
Alex Webster <alx.webster@gmail.com>
2021-11-03 14:37:42 -0600

    added summarized NM VCNP precip data
    precip has been summarized by watersheds we have grab samples from, as cumulative monthly, avg monthly, and avd daily

--- R/VCNP_ppt.R
+++ R/VCNP_ppt.R
@@ -17,6 +17,9 @@ library(zoo)
 library(xts)
 library(forecast)
 
+is.nan.data.frame <- function(x)
+  do.call(cbind, lapply(x, is.nan))
+
 #### load ppt data ####
 
 header = c("datetime","ppt_in","wind_speed_mph","wind_dir_deg","air_temp_F","fuel_temp_F","rel_humidity_perc","battery_volts","maxgust_dir_deg","maxgust_speed_mph","solar_rad_w.m2")
@@ -78,41 +81,50 @@ for(i in names(pptz)){
 }
 list2env(pptz,envir=.GlobalEnv)
 
+#### get hrly avg ####
 
-## round time stamps to nearest hour
-is.nan.data.frame <- function(x)
-  do.call(cbind, lapply(x, is.nan))
-## remove duplicates - consolidate duplicates by finding average
+## plot monthly avg from all freq vs hrly - should be similar
+# round time stamps to nearest hour
 pptz_h = pptz
 for(i in names(pptz_h)){
-  pptz_h[[i]]$datetimeMT = lubridate::round_date(pptz_h[[i]]$datetimeMT, "60 minutes") 
+  pptz_h[[i]]$datetimeMT = lubridate::round_date(pptz_h[[i]]$datetimeMT, "hour") 
   pptz_h[[i]] = 
     pptz_h[[i]] %>% 
     group_by(datetimeMT,station) %>% 
     dplyr::summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
   pptz_h[[i]][is.nan(pptz_h[[i]])]=NA
 }
-# plot to check hrly against raw data
-ppt_sites = c("redondo_ppt")
-pdf("figures/NM_ppt_rawVShr_comp.pdf", width=20, height=10) 
-par(mfrow=c(2,1))
-for(i in ppt_sites){
-  barplot(height = pptz[[i]][["ppt_in"]], names.arg = pptz[[i]][["datetimeMT"]],
-          xlab="Date", ylab="Precipitation (in)", main=i, ylim = c(0,1.5))
-  barplot(height = pptz_h[[i]][["ppt_in"]], names.arg = pptz_h[[i]][["datetimeMT"]],
-          xlab="Date", ylab="Precipitation (in)", main=i, xaxt = "n")
-  # xlim=c(as.Date("2003-10-24"), as.Date("2021-10-23"))  
-  #axis.POSIXct(1, at=seq(min(pptz_h[[i]][["datetimeMT"]]), max(pptz_h[[i]][["datetimeMT"]]),  by="years"), format="%m-%Y")
-  axis.POSIXct(1, at=pptz_h[[i]][["datetimeMT"]], labels=format(pptz_h[[i]][["datetimeMT"]], "%m/%d"))
+# get monthly avg from all freq
+pptz_allfreq_to_m = pptz
+for(i in names(pptz_allfreq_to_m)){
+  pptz_allfreq_to_m[[i]]$datetimeMT = lubridate::round_date(pptz_allfreq_to_m[[i]]$datetimeMT, "month") 
+  pptz_allfreq_to_m[[i]] = 
+    pptz_allfreq_to_m[[i]] %>% 
+    group_by(datetimeMT,station) %>% 
+    dplyr::summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
+  pptz_allfreq_to_m[[i]][is.nan(pptz_allfreq_to_m[[i]])]=NA
 }
-dev.off() 
+# get monthly avg from hrly
+pptz_hrly_to_m = pptz_h
+for(i in names(pptz_hrly_to_m)){
+  pptz_hrly_to_m[[i]]$datetimeMT = lubridate::round_date(pptz_hrly_to_m[[i]]$datetimeMT, "month") 
+  pptz_hrly_to_m[[i]] = 
+    pptz_hrly_to_m[[i]] %>% 
+    group_by(datetimeMT,station) %>% 
+    dplyr::summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
+  pptz_hrly_to_m[[i]][is.nan(pptz_hrly_to_m[[i]])]=NA
+}
+# compare
+dat1 = pptz_allfreq_to_m[["valletoledo_ppt"]]
+p1 = ggplot(data = dat1, aes(x=datetimeMT, y=ppt_in)) +
+  geom_col() + theme_classic() + scale_y_continuous(limits = c(0,0.005))
+dat2 = pptz_hrly_to_m[["valletoledo_ppt"]]
+p2 = ggplot(data = dat2, aes(x=datetimeMT, y=ppt_in)) +
+  geom_col() + theme_classic() + scale_y_continuous(limits = c(0,0.005))
+grid.arrange(p1,p2)
+
 # save to individual dfs
 list2env(pptz_h,envir=.GlobalEnv)
-
-dat = pptz_h[[i]]
-dat = dat[dat$datetimeMT>as.Date("2005-01-01") & dat$datetimeMT<as.Date("2005-02-01"),]
-ggplot(data = dat, aes(x=datetimeMT, y=ppt_in)) +
-  geom_col() + theme_classic()
 
 # ## remove outliers routine
 # # make new list for corrected data
@@ -149,38 +161,8 @@ ggplot(data = dat, aes(x=datetimeMT, y=ppt_in)) +
 # }
 
 
-#### plot ####
 
-range(ppt_all$ppt_in, na.rm = T)
-range(ppt_all$datetimeMT)
-
-ppt_sites = c("redondo_ppt", "hiddenvalley_ppt", "losposos_ppt", "sanantonio_ppt", "vallegrande_ppt", "valletoledo_ppt")
-pdf("figures/NM_ppt_all_stations.pdf", width=20, height=12) 
-par(mfrow=c(3,2))
-for(i in ppt_sites){
-  barplot(height = pptz[[i]][["ppt_in"]], names.arg = pptz[[i]][["datetimeMT"]],
-          xlab="Date", ylab="Precipitation (in)", main=i)
-  # xlim=c(as.Date("2003-10-24"), as.Date("2021-10-23"))  , xaxt = "n"
-  #axis(1, pptz[[i]][["datetimeMT"]], format(pptz[[i]][["datetimeMT"]], "%m %Y"), cex.axis = .7)
-}
-dev.off() 
-# 
-# ppt_all_p =
-#   ggplot(data = pptz[["ppt_all"]], aes(x=datetimeMT, y=ppt_in)) +
-#   # geom_point(aes(colour=station)) +
-#   # geom_line(aes(colour=station))+
-#   geom_bar(stat="identity") +
-#   facet_wrap(~station)
-#   #scale_color_brewer(palette="RdBu", direction = -1, name="Station")
-
-# # plot each
-# par(mfrow=c(1,1))
-# barplot(height = pptz[[1]][["ppt_in"]], names.arg = pptz[[1]][["datetimeMT"]],
-#         xlab="Date", ylab="Precipitation (in)",main = names(pptz)[1])
-# barplot(height = pptz[[1]][["ppt_in"]], names.arg = pptz[[1]][["datetimeMT"]],
-#         xlab="Date", ylab="Precipitation (in)",main = names(pptz)[1])
-
-#### average across sites ####
+#### average across ppt stations for each watershed ####
 
 # check avaiable date ranges in each site
 range(redondo_ppt$datetimeMT, na.rm = T)
@@ -198,7 +180,6 @@ range(valletoledo_ppt$datetimeMT, na.rm = T)
 # IND_AB	Indios Creek - above burn	                valletoledo	NA	          NA
 # RED	    Redondo Creek	                            redondo	    NA	          NA
 # RSAW	  San Antonio - West	                      sanantonio	NA	          NA
-
 # RSA	    San Antonio Creek - Toledo	              valletoledo	losposos	    NA
 # SULF	  Sulfur Creek                            	redondo	    sanantonio	  NA
 # EFJ	    East Fork Jemez River	                    losposos	  redondo	      vallegrande
@@ -222,12 +203,137 @@ ppt_RED$ID = "RED"
 
 ppt_RSAW = sanantonio_ppt
 ppt_RSAW$Grab_bgc_name = "San Antonio - West"
-ppt_RED$ID = "RSAW"
+ppt_RSAW$ID = "RSAW"
 
-## create clean set of time stamps to join to ##
-time <- data.frame(
-  datetimeMT = seq.POSIXt(
-    from = ISOdatetime(2004,07,15,12,0,0, tz = "Etc/GMT+7"),
-    to = ISOdatetime(2021,10,22,15,00,0, tz= "Etc/GMT+7"),
-    by = "15 min" ))
+ppt_RSA = 
+  bind_rows(valletoledo_ppt, losposos_ppt) %>%
+  group_by(datetimeMT) %>%   
+  dplyr::summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
+ppt_RSA$Grab_bgc_name = "San Antonio Creek - Toledo"
+ppt_RSA$ID = "RSA"
+ppt_RSA = tibble::add_column(ppt_RSA, station = "avg_valletoledo_losposos", .after = "datetimeMT")
+ppt_RSA[is.nan(ppt_RSA)]=NA
 
+ppt_SULF = 
+  bind_rows(redondo_ppt, sanantonio_ppt) %>%
+  group_by(datetimeMT) %>%   
+  dplyr::summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
+ppt_SULF$Grab_bgc_name = "Sulfur Creek"
+ppt_SULF$ID = "SULF"
+ppt_SULF = tibble::add_column(ppt_SULF, station = "avg_redondo_sanantonio", .after = "datetimeMT")
+ppt_SULF[is.nan(ppt_SULF)]=NA
+
+ppt_EFJ = 
+  bind_rows(redondo_ppt, losposos_ppt, vallegrande_ppt) %>%
+  group_by(datetimeMT) %>%   
+  dplyr::summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
+ppt_EFJ$Grab_bgc_name = "East Fork Jemez River"
+ppt_EFJ$ID = "EFJ"
+ppt_EFJ = tibble::add_column(ppt_EFJ, station = "avg_losposos_redondo_vallegrande", .after = "datetimeMT")
+ppt_EFJ[is.nan(ppt_EFJ)]=NA
+
+## make df of all
+# make columns match
+cols_to_keep <- intersect(colnames(ppt_RED), colnames(ppt_EFJ))
+ppt_IND_BB <- ppt_IND_BB[,cols_to_keep, drop=FALSE]
+ppt_IND <- ppt_IND[,cols_to_keep, drop=FALSE]
+ppt_IND_AB <- ppt_IND_AB[,cols_to_keep, drop=FALSE]
+ppt_RSAW <- ppt_RSAW[,cols_to_keep, drop=FALSE]
+ppt_RSA <- ppt_RSA[,cols_to_keep, drop=FALSE]
+ppt_SULF <- ppt_SULF[,cols_to_keep, drop=FALSE]
+ppt_EFJ <- ppt_EFJ[,cols_to_keep, drop=FALSE]
+ppt_all = rbind(ppt_IND_BB,ppt_IND,ppt_IND_AB,ppt_RED,ppt_RSAW,ppt_RSA,ppt_SULF,ppt_EFJ)
+
+pptz_h_ws = list(ppt_IND_BB,ppt_IND,ppt_IND_AB,ppt_RSAW,ppt_RSA,ppt_SULF,ppt_EFJ,ppt_all)
+names(pptz_h_ws) = c("ppt_IND_BB","ppt_IND","ppt_IND_AB","ppt_RSAW","ppt_RSA","ppt_SULF","ppt_EFJ","ppt_all")
+
+#
+#### get monthly cumulative (sum) ####
+
+ppt_all[which(is.na(ppt_all$ppt_in)),]
+# 4 NAs... don't seem worth worrying about for calculating sums
+
+pptz_m = pptz_h_ws
+for(i in names(pptz_m)){
+  pptz_m[[i]]$datetimeMT = lubridate::round_date(pptz_m[[i]]$datetimeMT, "month") 
+  pptz_m[[i]] = 
+    pptz_m[[i]] %>% 
+    group_by(datetimeMT,ID) %>% 
+    dplyr::summarize(cumulative_precip_in = sum(ppt_in, na.rm = TRUE)) %>%
+    ungroup()
+  pptz_m[[i]][is.nan(pptz_m[[i]])]=NA
+  # convert inches to mm
+  pptz_m[[i]]$cumulative_precip_mm = pptz_m[[i]]$cumulative_precip_in*25.4
+}
+
+# pull out df of all sites
+ppt_cum_m = pptz_m[["ppt_all"]]
+
+# plot 
+ppt_cum_m_p =
+  ggplot(data = ppt_cum_m, aes(x=datetimeMT, y=cumulative_precip_mm)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~ID)
+
+# save df of all sites
+saveRDS(ppt_cum_m, "data_working/VCNPprecip_m_cum_edited_110321.rds")
+
+#### get daily average ####
+
+ppt_all[which(is.na(ppt_all$ppt_in)),]
+# 4 NAs... don't seem worth worrying about for calculating sums
+
+pptz_d = pptz_h_ws
+for(i in names(pptz_d)){
+  pptz_d[[i]]$datetimeMT = lubridate::round_date(pptz_d[[i]]$datetimeMT, "day") 
+  pptz_d[[i]] = 
+    pptz_d[[i]] %>% 
+    group_by(datetimeMT,ID) %>% 
+    dplyr::summarize(cumulative_precip_in = mean(ppt_in, na.rm = TRUE)) %>%
+    ungroup()
+  pptz_d[[i]][is.nan(pptz_d[[i]])]=NA
+  # convert inches to mm
+  pptz_d[[i]]$cumulative_precip_mm = pptz_d[[i]]$cumulative_precip_in*25.4
+}
+
+# pull out df of all sites
+ppt_avg_d = pptz_d[["ppt_all"]]
+
+# plot 
+ppt_avg_dp =
+  ggplot(data = ppt_avg_d, aes(x=datetimeMT, y=cumulative_precip_mm)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~ID)
+
+# save df of all sites
+saveRDS(ppt_avg_d, "data_working/VCNPprecip_d_avg_edited_110321.rds")
+
+#### get monthly average ####
+
+ppt_all[which(is.na(ppt_all$ppt_in)),]
+# 4 NAs... don't seem worth worrying about for calculating sums
+
+pptz_m_avg = pptz_h_ws
+for(i in names(pptz_m_avg)){
+  pptz_m_avg[[i]]$datetimeMT = lubridate::round_date(pptz_m_avg[[i]]$datetimeMT, "month") 
+  pptz_m_avg[[i]] = 
+    pptz_m_avg[[i]] %>% 
+    group_by(datetimeMT,ID) %>% 
+    dplyr::summarize(cumulative_precip_in = mean(ppt_in, na.rm = TRUE)) %>%
+    ungroup()
+  pptz_m_avg[[i]][is.nan(pptz_m_avg[[i]])]=NA
+  # convert inches to mm
+  pptz_m_avg[[i]]$cumulative_precip_mm = pptz_m_avg[[i]]$cumulative_precip_in*25.4
+}
+
+# pull out df of all sites
+ppt_avg_m = pptz_m_avg[["ppt_all"]]
+
+# plot 
+ppt_avg_m_p =
+  ggplot(data = ppt_avg_m, aes(x=datetimeMT, y=cumulative_precip_mm)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~ID)
+
+# save df of all sites
+saveRDS(ppt_avg_m, "data_working/VCNPprecip_m_avg_edited_110321.rds")
