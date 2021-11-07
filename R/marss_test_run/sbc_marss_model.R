556bc4524955b1b87ebc27169a24a1573fe3181a
Heili Lowman <heili.lowman@gmail.com>
2021-11-07 10:22:12 -0800

    Working MARSS model!!

--- R/marss_test_run/sbc_marss_model.R
+++ R/marss_test_run/sbc_marss_model.R
@@ -8,7 +8,7 @@
 # S - State
 # S - Space
 
-# Note: As of November 3, I've been encountering issues with missing data so this first go-round, trying to make it work with just 2 sites at which I've verified the precip and fire covariate data is complete.
+# Note: As of November 3, I've been encountering issues with missing data so this first go-round, trying to make it work with just a few sites at which I've verified the precip and fire covariate data is complete.
 
 #### Setup ####
 
@@ -30,6 +30,19 @@ location <- read_csv("data_raw/sbc_sites_stream_hydro.csv")
 
 #### Filter & Joining Data ####
 
+# First, to avoid strange gaps in data, I'm creating a dummy column with the dates of interest.
+# And I need to do this for each site, since otherwise it'll allow gaps.
+dates1 <- data.frame(seq(as.Date("2002/9/1"), by = "month", length.out = 166)) %>%
+  rename(Date = 'seq.as.Date..2002.9.1....by....month...length.out...166.') %>%
+  mutate(site = "HO00")
+
+dates2 <- data.frame(seq(as.Date("2002/9/1"), by = "month", length.out = 166)) %>%
+  rename(Date = 'seq.as.Date..2002.9.1....by....month...length.out...166.') %>%
+  mutate(site = "RG01")
+## by month from 9/1/2002 to 7/1/2016
+
+dates <- rbind(dates1, dates2)
+
 # And to join the datasets, I need to identify the matching stream sites for the precip data
 precip_ed <- precip %>%
   mutate(sitecode_match = factor(case_when(
@@ -45,34 +58,39 @@ precip_ed <- precip %>%
     sitecode == "SBEB" ~ "MC00",
     sitecode == "ELDE" ~ "RS02"))) %>%
   rename(sitecode_precip = sitecode,
-         site_precip = site)
+         site_precip = site) %>%
+  mutate(Day = 1) %>% # new column of "days"
+  mutate(Date = make_date(Year, Month, Day))
 
 # Joining together with the stream chemistry and wildfire datasets.
+# First, join precip with the dates.
+dates_precip <- left_join(dates, precip_ed, by = c("Date", "site" = "sitecode_match"))
 # left join with precip so as not to lose any data based on fewer chemistry measurements
-precip_chem <- left_join(precip_ed, chem, by = c("Year", "Month", "sitecode_match" = "site_code"))
+precip_chem <- left_join(dates_precip, chem, by = c("Year", "Month", "site" = "site_code"))
 # and again left join with the larger dataset so as not to accidentally drop records
-dat <- left_join(precip_chem, fire, by = c("sitecode_match" = "site_code", "Year", "Month")) %>%
-  mutate(Day = 1) %>%
-  mutate(Date = make_date(Year, Month, Day)) # And add dates
-# Yay! :)
+dat <- left_join(precip_chem, fire, by = c("Year", "Month", "site" = "site_code"))
 
 # And, for this first attempt to try and get the MARSS model working, I'm going to filter down to only HO00 and RG01 sites.
 dat_2 <- dat %>%
   filter(site %in% c("HO00", "RG01"))
 
+# The trimming below is no longer necessary since we created a primary column with this same date range at the beginning of this script.
 # Now, to check the timeframes of the data available so we can trim down to comparable timespans.
-date_check <- dat_2 %>%
-  group_by(site) %>%
-  summarize(minDate = min(Date), maxDate = max(Date))
+# date_check <- dat_2 %>%
+#   group_by(site) %>%
+#   summarize(minDate = min(Date), maxDate = max(Date))
 
 # Ok, so need to trim to start date of 9/2002 and end date of 07/2016.
-dat_2_trim <- dat_2 %>%
-  filter(Date >= "2002-09-01") %>%
-  filter(Date <= "2016-07-01")
+# dat_2_trim <- dat_2 %>%
+#   filter(Date >= "2002-09-01") %>%
+#   filter(Date <= "2016-07-01")
 
 # And, inspect dataset for missing covariate data.
-sum(is.na(dat_2_trim$fire)) # 0
-sum(is.na(dat_2_trim$cumulative_precip_mm)) # 0
+sum(is.na(dat_2$fire)) # 0
+sum(is.na(dat_2$cumulative_precip_mm)) # 0
+# Great!
+
+# Note for future me - be VERY careful with the joining above. Something weird was happening previously where precip data that is present was simply dropping off.
 
 #### Model fit ####
 
@@ -82,22 +100,18 @@ sum(is.na(dat_2_trim$cumulative_precip_mm)) # 0
 # Starting with NH4 for test run.
 
 # Note: Not scaling for now, but this should also be added in later.
-dat_nh4 <- dat_2_trim %>%
+dat_nh4 <- dat_2 %>%
   select(site, Year, Month, mean_nh4_uM, cumulative_precip_mm, fire) %>%
   pivot_wider(names_from = site, values_from = c(mean_nh4_uM, cumulative_precip_mm, fire)) %>%
   #log() %>% # takes the log
   #scale(scale = FALSE) %>% # centers columns of a numeric matrix
   t() # transposes data
 
-# STILL getting NAs?!?
-# Ok, so I may need to start with a blank dataframe of the desired date range and then merge everything based on that...
-#### STOPPED HERE NOVEMBER 3 ####
-
 # Pull out only NH4 data
-dat_nh4_ed <- dat_nh4[3:6,]
+dat_dep <- dat_nh4[3:4,]
 
 # Make covariate inputs
-dat_cov <- dat_nh4[c(1:2,7:14),]
+dat_cov <- dat_nh4[c(1:2,5:8),]
 
 # Model setup
 mod_list <- list(
@@ -112,12 +126,13 @@ mod_list <- list(
 )
 
 # Fit model
-fit_nh4 <- MARSS(y = dat_nh4_ed, model = mod_list,
+fit <- MARSS(y = dat_dep, model = mod_list,
                  control = list(maxit = 5000), method = "BFGS")
+
+# Still getting the following message:
 #MARSS: NaNs in data are being replaced with NAs.  There might be a problem if NaNs shouldn't be in the data.
 #NA is the normal missing value designation.
-#Errors were caught in checkModelList 
-#model$c is a matrix. No NAs or Infs allowed in this case.
-#Error: Stopped in checkModelList() due to specification problem(s).
+
+# It worked!!!
 
 # End of script.
