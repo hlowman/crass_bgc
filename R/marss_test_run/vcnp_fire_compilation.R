8c2485b10ed5868c418ca22050b8a089deca763c
bets-summers <83677618+bets-summers@users.noreply.github.com>
2021-11-15 10:12:43 -0700

    Adding fire variables to VCNP data

--- R/marss_test_run/vcnp_fire_compilation.R
+++ R/marss_test_run/vcnp_fire_compilation.R
@@ -9,13 +9,54 @@
 # 2) Thompson Ridge fire (start date: 31 May, 2013)
 
 
-# Load packages
+## Load packages
 library(plyr) # needs to be loaded prior to dplyr
 library(tidyverse) # contains dplyr
 library(lubridate)
 library(here)
 
 
-# May not need chem data but was thinking to merge with fire data
+## May not need chem data but was thinking to merge with fire data
 chem_reg_vcnp <- read_rds("data_working/VCNPchem_edited_110821.rds")
+
+str(chem_reg_vcnp)
+list(chem_reg_vcnp$site_code)
+
+chem_reg_vcnp <- chem_reg_vcnp %>% rename(site = "site_code") # rename to site to match sbc variable name
+
+## Tidy site_code variable list:
+# 1) Remove rows with Nas
+chem_reg_vcnp <- chem_reg_vcnp %>% 
+  filter(!is.na(site)) %>%
+  filter(!is.na(Date)) %>%
+  filter(site != "La Jara Well - HQ Drinking Water, Pre-treatment") # remove random site, not stream
+
+# 2) Need to fix variable names because inconsistent naming (location of dashes)
+
+
+
+## Fire names and start dates of burn
+# Las Conchas (2011-06-26) 
+# Thompson Ridge (2013-05-31)
+
+
+## Add in columns for the fires based on start dates:
+# 0 - denotes pre-fire months
+# 1 - denotes post-fire months
+# Each fire will get its own column, so that effects can potentially be additive
+# Or so we can combine decay functions later
+
+# currently use Date variable to determine when site is 1 or 0.
+dates_fire_vcnp <- chem_reg_vcnp %>%
+  mutate(LasConchas = ifelse(Date >= "2011-06-26", 1, 0),
+         ThompsonRidge = ifelse(Date >= "2013-05-31", 1, 0))
+
+## Need information on what sites were within the burn area or downstream of burn area to better designate if a site gets a 1 or 0 after fire start date. 
+# Sites not impacted by wildfire just receive a 0. 
+
+
+# And export for MARSS script
+saveRDS(dates_fire, "data_working/VCNPfire_edited_11151021.rds")
+
+# End of script.
 
