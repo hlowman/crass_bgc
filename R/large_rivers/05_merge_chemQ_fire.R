10d799feb66c8e70f9b7a183d6eed84c1430d20e
Tamara <tkharms@alaska.edu>
2022-10-14 17:31:21 -0800

    merge fire attributes with chem & Q

--- R/large_rivers/05_merge_chemQ_fire.R
+++ R/large_rivers/05_merge_chemQ_fire.R
@@ -63,13 +63,59 @@ fires <- bind_rows(singletons, fires)
 # Join to catchments_summary for cumulative burn extent
 fires <- left_join(fires, catchments_summary)
 
-# Set fire year to 1940 for unburned catchments. This is the temporal extent of the BLM fire perimeter map source of fire data
-fires <- fires %>% mutate_at(c("fireyear_rct", "fireyear_lg"), ~ifelse(fireyear_rct == 0, 1940, .)) %>%
-  mutate_at(vars(fireyear_lg), ~ifelse(fireyear_lg == 0, 1940, .))
+# re-format dates
 
-write.csv(fires, "spatial_munged.csv", row.names = FALSE)
+# Set fire year to 1980 for unburned catchments. This is the temporal extent of the MTBS fire data source
+# note: using dplyr if_else because base ifelse strips off formatting
+fires <- fires %>% ungroup() %>%
+                   mutate_at(c("Date_rct", "Date_lg"), ~if_else(is.na(Date_rct), as.Date("1979-12-31"), .)) 
+
+fires %>% select("usgs_site") %>% n_distinct()
+# 1022 catchments
+
+chemQ %>% select("usgs_site") %>% n_distinct()
+# 1016. Not all catchments have data for the particular chems selected
+
+fires %>% ggplot(aes(x = pctburn_lg)) +
+          geom_histogram(color = "red") +
+          geom_histogram(aes(x = pctburn_rct), color = "blue")
+  
+fires %>% ggplot(aes(x = pctburn_lg)) +
+  geom_histogram(color = "red")
+
+fires %>% ggplot(aes(x = pctburn_rct)) +
+  geom_histogram(color = "blue")
+
+fires %>% group_by(usgs_site) %>% filter(!is.na(pctburn_lg)) %>% nrow()
+# 651 catchments with fires... seems like a lot?
+
+fires %>% filter(is.na(pctburn_lg)) %>% nrow()
+# 375... seems like too few? ratio of burned:unburned catchments seems skewed
+
+write.csv(fires, here("USGS_data", "USGS_spatial_fires.csv"), row.names = FALSE)
+
+### Merge fire data with chemQ ###
+chemQsp <- left_join(chemQ, fires, by = "usgs_site")
+
+# Quick plot to check
+chemQsp %>% filter(!is.na(Incid_Name_lg) & CharacteristicName == "Nitrate" & Date_UTC > "2021-05-01") %>%
+            ggplot(aes(x = pctburn_lg, y = mn_value_std)) +
+            geom_point()
+
+### Summary metrics ###
+## Time since fire in fractional years
+# recent fire
+
+# largest fire
+
+# pre/post burn column
 
 ### Plot C-Q pre/post burn ###
+# cumulative fire vs. recent mean chem
+
+######
+# C-Q pre-post burn for burned catchments (how long post-burn window?): same year, 1 y, 2, y, 3 y, etc.
+# time series for burned catchments
 
 ### Next scripts
-## Then apply filters: size, Now add LULC filter & replot C-Q
+## Then apply filters: catchment size, Now add LULC filter & replot C-Q
