3eeee548c511231faa0cb6c8c4a54921d0bfa00b
Heili Lowman <heili.lowman@gmail.com>
2023-02-07 12:11:34 -0500

    Made new NH4 time series figure.

---
+++ R/marss_test_run/sbc_solutes_figures.R
@@ -0,0 +1,62 @@
+# SB MARSS models for other solutes
+# Script started February 7, 2023
+# Heili Lowman, Alex Webster
+
+# The following script will be used to create the timeseries figure
+# to be added in the box with nutrient responses to fire in the SBC
+# watersheds ONLY.
+
+# Setup
+library(tidyverse)
+library(lubridate)
+library(MARSS)
+library(naniar)
+library(emoGG)
+library(calecopal)
+
+# Load in data used for MARSS models.
+df <- readRDS("data_working/marss_data_sb_N_P_vwm_011723.rds")
+
+# Need to make new columns for designating pre- and post-fire dates
+# for plotting purposes.
+df <- df %>%
+  mutate(day = 1) %>%
+  mutate(date = make_date(year, month, day)) %>%
+  # if multiple fires in a watershed occurred, I am coloring based
+  # on the first of the fires to occur
+  mutate(post_fire = case_when(site == "AB00" & date > as.Date("2009-05-01") |
+                                 site == "GV01" & date > as.Date("2004-06-01") |
+                                 site == "HO00" & date > as.Date("2004-06-01") |
+                                 site == "MC06" & date > as.Date("2008-11-01") |
+                                 site == "RS02" & date > as.Date("2008-11-01") ~ "A",
+                               TRUE ~ "B")) %>%
+  # and re-order sites for better plotting of sequential fires
+  mutate(site_factor = factor(site, levels = c("GV01", "HO00", "MC06", "RS02", "AB00")))
+
+# Create figure.
+(nh4_fig <- ggplot(df, aes(x = date, y = vwm_nh4, color = site, alpha = post_fire)) +
+  geom_point(size = 5) +
+  geom_line() +
+  scale_y_log10() +
+  scale_color_manual(values = cal_palette("figmtn"), guide = "none") +
+  scale_alpha_manual(values = c(1, 0.3), guide = "none") +
+  labs(x = "Date", 
+       y = "Volume-Weighted Mean Monthly Ammonium Concentration",
+       color = "Site") +
+  facet_grid(site_factor~.) +
+  theme_bw() +
+  theme(axis.title = element_text(size = 24),
+        axis.text = element_text(size = 20),
+        strip.text.x = element_text(size = 24),
+        strip.text.y = element_text(size = 24),
+        strip.background = element_rect(colour="white", fill="white")))
+
+# Export plot.
+ggsave(("TS_SB_NH4_020723.png"),
+       path = "figures",
+       width = 30,
+       height = 30,
+       units = "cm"
+)
+
+# End of script.
