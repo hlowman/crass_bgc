66fadb93b804d1bae37d5cdd2c086aa2ccc14740
AJW <alx.webster@gmail.com>
2023-08-07 14:45:26 -0600

    VCNP sonde data script added. Data is already in raw_data

---
+++ R/VCNP_sonde_SpC.R
@@ -0,0 +1,195 @@
+#### read me ####
+# the purpose of this script is to put together some timeseries of sonde data from VCNP. 
+# Data was provided by Mic from the Crossey lab off of their Aquarius data logs. 
+# Data has been appended (stitched together) but may not be QAQCed
+
+#### load libraries ####
+
+library(lubridate)
+library(tidyverse)
+library(gridExtra)
+library(zoo)
+library(xts)
+library(lubridate)
+
+#
+#### load data and clean up ####
+
+sondez = readRDS("SondeData_AlexWebster/VCNP_sonde.rds")
+
+
+#### extract and smooth SpC data ####
+
+# going to start with just EFJ
+
+EFJ_SpC = sondez[["EFJ"]] %>% 
+  select(datetime_NM, SPC_uS_Value)
+
+summary(EFJ_SpC)
+# NA's   :0
+
+# remove zeros, negative values, and one extreme value
+EFJ_SpC$SPC_uS_Value[EFJ_SpC$SPC_uS_Value<1] = NA
+EFJ_SpC$SPC_uS_Value[EFJ_SpC$SPC_uS_Value==0] = NA
+EFJ_SpC$SPC_uS_Value[EFJ_SpC$SPC_uS_Value>1000] = NA
+
+# Make univariate zoo time series#
+EFJ_SpC_ts<-read.zoo(EFJ_SpC, index.column=1, format="%Y-%m-%d %H:%M:%S", tz="America/Denver")
+# try smoothing to reduce noise
+plot(EFJ_SpC_ts, type="l",main='Simple Moving Average (SMA)', ylim=c(-1,1000))
+lines(rollmean(EFJ_SpC_ts,8), type="l",col='red')
+# looks better
+
+# turn smoothed ts back into df
+EFJ_SpC_ts_sm = rollmean(EFJ_SpC_ts,8)
+# revert back to df
+EFJ_SpC_sm = as.data.frame(EFJ_SpC_ts_sm)
+EFJ_SpC_sm$datetime_NM = as.POSIXct(row.names(EFJ_SpC_sm), tz="America/Denver")
+names(EFJ_SpC_sm) = c("SPC_uS_Value","datetime_NM")
+
+# plot by year
+EFJ_SpC_sm$yr = year(EFJ_SpC_sm$datetime_NM)
+EFJ_SpC_sm$doy = yday(EFJ_SpC_sm$datetime_NM)
+ggplot(data= EFJ_SpC_sm, aes(y=SPC_uS_Value, x=doy))+
+  geom_line()+
+  facet_wrap(~yr)
+
+
+
+###  JCP
+
+JCP_SpC = sondez[["JCP"]] %>% 
+  select(datetime_NM, SPC_uS_Value)
+
+summary(JCP_SpC)
+# NA's   :10134
+
+# remove zeros, negative values, and one extreme value
+JCP_SpC$SPC_uS_Value[JCP_SpC$SPC_uS_Value<1] = NA
+JCP_SpC$SPC_uS_Value[JCP_SpC$SPC_uS_Value==0] = NA
+JCP_SpC$SPC_uS_Value[JCP_SpC$SPC_uS_Value>1000] = NA
+
+# Make univariate zoo time series#
+JCP_SpC_ts<-read.zoo(JCP_SpC, index.column=1, format="%Y-%m-%d %H:%M:%S", tz="America/Denver")
+# try smoothing to reduce noise
+plot(JCP_SpC_ts, type="l",main='Simple Moving Average (SMA)', ylim=c(-1,1000))
+lines(rollmean(JCP_SpC_ts,8), type="l",col='red')
+# looks better
+
+# turn smoothed ts back into df
+JCP_SpC_ts_sm = rollmean(JCP_SpC_ts,8)
+# revert back to df
+JCP_SpC_sm = as.data.frame(JCP_SpC_ts_sm)
+JCP_SpC_sm$datetime_NM = as.POSIXct(row.names(JCP_SpC_sm), tz="America/Denver")
+names(JCP_SpC_sm) = c("SPC_uS_Value","datetime_NM")
+
+# plot by year
+JCP_SpC_sm$yr = year(JCP_SpC_sm$datetime_NM)
+JCP_SpC_sm$doy = yday(JCP_SpC_sm$datetime_NM)
+ggplot(data= JCP_SpC_sm, aes(y=SPC_uS_Value, x=doy))+
+  geom_line()+
+  facet_wrap(~yr)
+
+
+###  RED
+
+RED_SpC = sondez[["RED"]] %>% 
+  select(datetime_NM, SPC_uS_Value)
+
+summary(RED_SpC)
+# NA's   :2
+
+# remove zeros, negative values, and one extreme value
+RED_SpC$SPC_uS_Value[RED_SpC$SPC_uS_Value<1] = NA
+RED_SpC$SPC_uS_Value[RED_SpC$SPC_uS_Value==0] = NA
+RED_SpC$SPC_uS_Value[RED_SpC$SPC_uS_Value>1000] = NA
+
+# Make univariate zoo time series#
+RED_SpC_ts<-read.zoo(RED_SpC, index.column=1, format="%Y-%m-%d %H:%M:%S", tz="America/Denver")
+# try smoothing to reduce noise
+plot(RED_SpC_ts, type="l",main='Simple Moving Average (SMA)', ylim=c(-1,1000))
+lines(rollmean(RED_SpC_ts,8), type="l",col='red')
+# looks better
+
+# turn smoothed ts back into df
+RED_SpC_ts_sm = rollmean(RED_SpC_ts,8)
+# revert back to df
+RED_SpC_sm = as.data.frame(RED_SpC_ts_sm)
+RED_SpC_sm$datetime_NM = as.POSIXct(row.names(RED_SpC_sm), tz="America/Denver")
+names(RED_SpC_sm) = c("SPC_uS_Value","datetime_NM")
+
+# plot by year
+RED_SpC_sm$yr = year(RED_SpC_sm$datetime_NM)
+RED_SpC_sm$doy = yday(RED_SpC_sm$datetime_NM)
+ggplot(data= RED_SpC_sm, aes(y=SPC_uS_Value, x=doy))+
+  geom_line()+
+  facet_wrap(~yr)
+
+
+###  RSAW
+
+RSAW_SpC = sondez[["RSAW"]] %>% 
+  select(datetime_NM, SPC_uS_Value)
+
+summary(RSAW_SpC)
+# NA's   :0
+
+# remove zeros, negative values, and one extreme value
+RSAW_SpC$SPC_uS_Value[RSAW_SpC$SPC_uS_Value<1] = NA
+RSAW_SpC$SPC_uS_Value[RSAW_SpC$SPC_uS_Value==0] = NA
+RSAW_SpC$SPC_uS_Value[RSAW_SpC$SPC_uS_Value>1000] = NA
+
+# Make univariate zoo time series#
+RSAW_SpC_ts<-read.zoo(RSAW_SpC, index.column=1, format="%Y-%m-%d %H:%M:%S", tz="America/Denver")
+# try smoothing to reduce noise
+plot(RSAW_SpC_ts, type="l",main='Simple Moving Average (SMA)', ylim=c(-1,1000))
+lines(rollmean(RSAW_SpC_ts,8), type="l",col='red')
+# looks better
+
+# turn smoothed ts back into df
+RSAW_SpC_ts_sm = rollmean(RSAW_SpC_ts,8)
+# revert back to df
+RSAW_SpC_sm = as.data.frame(RSAW_SpC_ts_sm)
+RSAW_SpC_sm$datetime_NM = as.POSIXct(row.names(RSAW_SpC_sm), tz="America/Denver")
+names(RSAW_SpC_sm) = c("SPC_uS_Value","datetime_NM")
+
+# plot by year
+RSAW_SpC_sm$yr = year(RSAW_SpC_sm$datetime_NM)
+RSAW_SpC_sm$doy = yday(RSAW_SpC_sm$datetime_NM)
+ggplot(data= RSAW_SpC_sm, aes(y=SPC_uS_Value, x=doy))+
+  geom_line()+
+  facet_wrap(~yr)
+
+###  RSA
+
+RSA_SpC = sondez[["RSA"]] %>% 
+  select(datetime_NM, SPC_uS_Value)
+
+summary(RSA_SpC)
+# NA's   :10819
+
+# remove zeros, negative values, and one extreme value
+RSA_SpC$SPC_uS_Value[RSA_SpC$SPC_uS_Value<1] = NA
+RSA_SpC$SPC_uS_Value[RSA_SpC$SPC_uS_Value==0] = NA
+RSA_SpC$SPC_uS_Value[RSA_SpC$SPC_uS_Value>1000] = NA
+
+# Make univariate zoo time series#
+RSA_SpC_ts<-read.zoo(RSA_SpC, index.column=1, format="%Y-%m-%d %H:%M:%S", tz="America/Denver")
+# try smoothing to reduce noise
+plot(RSA_SpC_ts, type="l",main='Simple Moving Average (SMA)', ylim=c(-1,1000))
+lines(rollmean(RSA_SpC_ts,8), type="l",col='red')
+# looks better
+
+# turn smoothed ts back into df
+RSA_SpC_ts_sm = rollmean(RSA_SpC_ts,8)
+# revert back to df
+RSA_SpC_sm = as.data.frame(RSA_SpC_ts_sm)
+RSA_SpC_sm$datetime_NM = as.POSIXct(row.names(RSA_SpC_sm), tz="America/Denver")
+names(RSA_SpC_sm) = c("SPC_uS_Value","datetime_NM")
+
+# plot by year
+RSA_SpC_sm$yr = year(RSA_SpC_sm$datetime_NM)
+RSA_SpC_sm$doy = yday(RSA_SpC_sm$datetime_NM)
+ggplot(data= RSA_SpC_sm, aes(y=SPC_uS_Value, x=doy))+
+  geom_line()+
+  facet_wrap(~yr)
