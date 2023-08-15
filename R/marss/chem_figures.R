1ef2e331b23655811088d856a44348284e94c9b8
Heili Lowman <heili.lowman@gmail.com>
2023-08-15 10:40:57 -0400

    Generated a few summary statistics to add to results section.

--- R/marss/chem_figures.R
+++ R/marss/chem_figures.R
@@ -36,11 +36,10 @@ df <- df %>%
   mutate(site_factor = factor(site, levels = c("GV01", "HO00", "RS02", "AB00")))
 
 # Create figure.
-(nh4_fig <- ggplot(df, aes(x = date, y = vwm_nh4, color = site_factor, alpha = post_fire)) +
-  geom_point(size = 5) +
-  geom_line() +
+(nh4_fig <- ggplot(df, aes(x = date, y = vwm_nh4, alpha = post_fire)) +
+  geom_point(size = 5, color = "#6592D6") +
+  geom_line(color = "#6592D6") +
   scale_y_log10() +
-  scale_color_manual(values = cal_palette("sbchannel"), guide = "none") +
   scale_alpha_manual(values = c(1, 0.3), guide = "none") +
   labs(x = "Date", 
        y = "Volume-Weighted Mean Monthly Ammonium Concentration",
@@ -54,7 +53,7 @@ df <- df %>%
         strip.background = element_rect(colour="white", fill="white")))
 
 # Export plot.
-# ggsave(("TS_SB_NH4_081123.png"),
+# ggsave(("TS_SB_NH4_081523.png"),
 #        path = "figures",
 #        width = 30,
 #        height = 30,
@@ -86,12 +85,9 @@ df_cond <- df_cond %>%
 # Create figure.
 (cond_fig_vc <- ggplot(df_cond %>%
                          filter(region == "VC"), 
-                       aes(x = date, y = mean_cond_uScm, 
-                           color = site_factor, alpha = post_fire)) +
-    geom_point(size = 5) +
-    geom_line() +
-    scale_color_manual(values = c("#BED6B3", "#92A587", "#4A5438", "#2F3525"), 
-                       guide = "none") +
+                       aes(x = date, y = mean_cond_uScm, alpha = post_fire)) +
+    geom_point(size = 5, color = "#92A587") +
+    geom_line(color = "#92A587") +
     scale_alpha_manual(values = c(1, 0.3), guide = "none") +
     labs(x = "Date", 
          y = "Mean Monthly Specific Conductivity") +
@@ -105,11 +101,9 @@ df_cond <- df_cond %>%
 
 (cond_fig_sb <- ggplot(df_cond %>%
                          filter(region == "SB"), 
-                       aes(x = date, y = mean_cond_uScm, 
-                           color = site_factor, alpha = post_fire)) +
-    geom_point(size = 5) +
-    geom_line() +
-    scale_color_manual(values = cal_palette("sbchannel"), guide = "none") +
+                       aes(x = date, y = mean_cond_uScm, alpha = post_fire)) +
+    geom_point(size = 5, color = "#6592D6") +
+    geom_line(color = "#6592D6") +
     scale_alpha_manual(values = c(1, 0.3), guide = "none") +
     labs(x = "Date") +
     facet_grid(site_factor~.) +
@@ -124,11 +118,58 @@ df_cond <- df_cond %>%
 (cond_fig <- cond_fig_vc + cond_fig_sb)
 
 # Export plot.
-ggsave(("TS_SB_VC_SpCond_081123.png"),
-       path = "figures",
-       width = 60,
-       height = 30,
-       units = "cm"
-)
+# ggsave(("TS_SB_VC_SpCond_081523.png"),
+#        path = "figures",
+#        width = 60,
+#        height = 30,
+#        units = "cm")
+
+#### Summary Stats ####
+
+# Running a few additional summary statistics to include in the results section.
+
+df_sb <- df_cond %>%
+  filter(region == "SB")
+
+df_sb_summary <- df_sb %>%
+  summarize(mean_cond = mean(mean_cond_uScm, na.rm = TRUE),
+            sd_cond = sd(mean_cond_uScm, na.rm = TRUE),
+            min_cond = min(mean_cond_uScm, na.rm = TRUE),
+            max_cond = max(mean_cond_uScm, na.rm = TRUE),
+            mean_ppt = mean(cumulative_precip_mm, na.rm = TRUE),
+            sd_ppt = sd(cumulative_precip_mm, na.rm = TRUE),
+            min_ppt = min(cumulative_precip_mm, na.rm = TRUE),
+            max_ppt = max(cumulative_precip_mm, na.rm = TRUE))
+
+unique(df_sb$fire_perc_ws) #  0.0 45.5  8.8 17.1  9.3 68.4
+
+df_vc <- df_cond %>%
+  filter(region == "VC")
+
+df_vc_summary <- df_vc %>%
+  summarize(mean_cond = mean(mean_cond_uScm, na.rm = TRUE),
+            sd_cond = sd(mean_cond_uScm, na.rm = TRUE),
+            min_cond = min(mean_cond_uScm, na.rm = TRUE),
+            max_cond = max(mean_cond_uScm, na.rm = TRUE),
+            mean_ppt = mean(cumulative_precip_mm, na.rm = TRUE),
+            sd_ppt = sd(cumulative_precip_mm, na.rm = TRUE),
+            min_ppt = min(cumulative_precip_mm, na.rm = TRUE),
+            max_ppt = max(cumulative_precip_mm, na.rm = TRUE))
+
+unique(df_vc$fire_perc_ws) #  0.0 37.0 21.7 42.8  7.8 79.9 93.5
+
+df_summary <- df %>%
+  summarize(mean_NH4 = mean(vwm_nh4, na.rm = TRUE),
+            sd_NH4 = sd(vwm_nh4, na.rm = TRUE),
+            min_NH4 = min(vwm_nh4, na.rm = TRUE),
+            max_NH4 = max(vwm_nh4, na.rm = TRUE),
+            mean_NO3 = mean(vwm_no3, na.rm = TRUE),
+            sd_NO3 = sd(vwm_no3, na.rm = TRUE),
+            min_NO3 = min(vwm_no3, na.rm = TRUE),
+            max_NO3 = max(vwm_no3, na.rm = TRUE),
+            mean_PO4 = mean(vwm_po4, na.rm = TRUE),
+            sd_PO4 = sd(vwm_po4, na.rm = TRUE),
+            min_PO4 = min(vwm_po4, na.rm = TRUE),
+            max_PO4 = max(vwm_po4, na.rm = TRUE))
 
 # End of script.
